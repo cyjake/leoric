237c48eac23251a87f63d8e255fe11b8a471f652
Chen Yangjian <252317+dotnil@users.noreply.github.com>
2018-10-30 11:14:05 +0800

    update Gemfile.lock & docs

--- test/prepare.sh
+++ test/prepare.sh
@@ -1,5 +1,3 @@
-mkdir -p tmp
-
 cat <<EOF | mysql -uroot
 CREATE DATABASE IF NOT EXISTS leoric;
 USE leoric;
@@ -8,5 +6,5 @@ EOF
 
 # SQLite perfers `AUTOINCREMENT` to `AUTO_INCREMENT`. Yet `AUTOINCREMENT` isn't available on columns not marked as `PRIMARY KEY`. As of `PRIMARY KEY`, with or without `AUTOINCREMENT` isn't much of difference.
 # - https://sqlite.org/autoinc.html
-sed 's/bigint(20) AUTO_INCREMENT/INTEGER/' test/dumpfile.sql | sqlite3 tmp/leoric.sqlite3
-npm rebuild sqlite3 --build-from-source
+sed 's/bigint(20) AUTO_INCREMENT/INTEGER/' test/dumpfile.sql | sqlite3 /tmp/leoric.sqlite3
+# npm rebuild sqlite3 --build-from-source
