3148463db274339fd85157acc44b2cce414763b6
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2020-01-25 23:08:49 +0800

    fix: date/time/datetime/timestamp, a temporary fix (#21)

--- test/unit/drivers/test.sqlite.js
+++ test/unit/drivers/test.sqlite.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const assert = require('assert').strict;
+const strftime = require('strftime');
+
 const { DataTypes } = require('../../..');
+const { heresql } = require('../../../lib/utils/string');
 const SqliteDriver = require('../../../lib/drivers/sqlite');
 
-const { INTEGER } = DataTypes;
+const { INTEGER, STRING, DATE } = DataTypes;
 
 const driver = new SqliteDriver('sqlite', {
   database: '/tmp/leoric.sqlite3',
@@ -41,5 +44,31 @@ describe('=> SQLite driver', () => {
       id: { type: INTEGER, primaryKey: true, autoIncrement: true },
       public: { type: INTEGER },
     });
+  });
+});
+
+describe('=> SQLite driver.query()', () => {
+  before(async () => {
+    await driver.dropTable('notes');
+    await driver.createTable('notes', {
+      id: { type: INTEGER, primaryKey: true },
+      title: STRING,
+      createdAt: DATE,
+    });
+  });
+
+  it('should handle timestamp correctly', async () => {
+    const createdAt = new Date();
+    await driver.query('INSERT INTO notes (title, created_at) VALUES (?, ?)', [
+      'Leah', createdAt,
+    ]);
+    const {
+      rows: [
+        { created_at }
+      ]
+    } = await driver.query(heresql(`
+      SELECT datetime(created_at, 'localtime') AS created_at FROM notes
+    `));
+    assert.equal(created_at, strftime('%Y-%m-%d %H:%M:%S', createdAt));
   });
 });
