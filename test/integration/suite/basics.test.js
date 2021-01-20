34345a20d5970ff2abc768e93f27e753edf13d3b
JimmyDaddy <heyjimmygo@gmail.com>
2021-01-20 21:02:58 +0800

    fix: instance[key] should not change after updating failed (#82)
    * fix: instance[key] should not change after updating failed
    
    * fix: instance._validateAttributes opt
    
    * fix: obj.changes bug
    
    * fix: validator revert value
    
    * Update lib/bone.js
    
    Co-authored-by: Chen Yangjian <252317+cyjake@users.noreply.github.com>
    
    * Update lib/validator.js
    
    Co-authored-by: Chen Yangjian <252317+cyjake@users.noreply.github.com>
    
    * fix: map to for-in
    
    * Update test/integration/suite/basics.test.js
    
    Co-authored-by: Chen Yangjian <252317+cyjake@users.noreply.github.com>
    
    * chore: fix unit test
    
    * chore: fix unit test
    
    Co-authored-by: Chen Yangjian <252317+cyjake@users.noreply.github.com>

--- test/integration/suite/basics.test.js
+++ test/integration/suite/basics.test.js
@@ -10,6 +10,10 @@ const Post = require('../../models/post');
 const TagMap = require('../../models/tagMap');
 const User = require('../../models/user');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 describe('=> Attributes', function() {
   beforeEach(async function() {
     await Post.remove({}, true);
@@ -138,14 +142,16 @@ describe('=> Attributes', function() {
     // should return false after first persist
     assert.equal(post.previousChanged(), false);
     post.extra = 'hello2';
+    await sleep(10);
     await post.save();
     // should return updated attributes' name after updating
-    assert.deepEqual(post.previousChanged(), ['extra']);
+    assert.deepEqual(post.previousChanged().sort(), ['extra', 'updatedAt']);
     post.title = 'monster hunter';
     post.extra = 'hello3';
+    await sleep(10);
     await post.save();
     // should return updated attributes' name after updating
-    assert.deepEqual(post.previousChanged().sort(), [ 'extra', 'title' ]);
+    assert.deepEqual(post.previousChanged().sort(), [ 'extra', 'title', 'updatedAt' ]);
   });
 
   it('Bone.previousChanges(key): raw VS rawPrevious', async function () {
@@ -164,8 +170,10 @@ describe('=> Attributes', function() {
     await post.save();
     assert.deepEqual(post.previousChanges(), {});
     post.title = 'MHW';
+    const prevUpdatedAt = post.updatedAt;
+    await sleep(10);
     await post.save();
-    assert.deepEqual(post.previousChanges(), { title: [ 'Untitled', 'MHW' ] });
+    assert.deepEqual(post.previousChanges(), { title: [ 'Untitled', 'MHW' ], updatedAt: [ prevUpdatedAt, post.updatedAt ] });
   });
 
   it('Bone.changes(key): raw VS rawSaved', async function () {
@@ -306,6 +314,13 @@ describe('=> Accessors', function() {
     expect(user.status).to.eql(1);
     expect(user.raw.status).to.equal(-1);
     await user.update({ status: 2 });
+    // set status(value = 0) {
+    //   this.attribute('status', value - 2);
+    // },
+    //  get status() {
+    //   const status = this.attribute('status');
+    //   return status + 2;
+    // }
     expect(user.status).to.eql(2);
     expect(user.raw.status).to.equal(0);
   });
