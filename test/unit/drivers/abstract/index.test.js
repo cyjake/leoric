89aa09ae42476de5138305fbe8279060d3ffd933
mijiu.zc <mijiu.zc@alibaba-inc.com>
2021-11-02 18:04:35 +0800

    style: lint style

--- test/unit/drivers/abstract/index.test.js
+++ test/unit/drivers/abstract/index.test.js
@@ -4,14 +4,14 @@ const assert = require('assert').strict;
 const { Logger } = require('../../../..');
 const AbstractDriver = require('../../../../src/drivers/abstract');
 
-describe('=> AbstractDriver#logger', function (){
-  it('should create logger by default', async function (){
+describe('=> AbstractDriver#logger', function() {
+  it('should create logger by default', async function() {
     const driver = new AbstractDriver();
     assert.ok(driver.logger);
     assert.ok(driver.logger instanceof Logger);
   });
 
-  it('should accept custom logger', async function (){
+  it('should accept custom logger', async function() {
     class CustomLogger extends Logger {};
     const driver = new AbstractDriver({ logger: new CustomLogger });
     assert.ok(driver.logger);
