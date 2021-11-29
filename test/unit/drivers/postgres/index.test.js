693cbceaf210a2b77ef50df1ebe206b056df5853
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2021-11-29 17:28:16 +0800

    breaking: logQueryError(err, sql, duration, options) (#237)

--- test/unit/drivers/postgres/index.test.js
+++ test/unit/drivers/postgres/index.test.js
@@ -38,7 +38,7 @@ describe('=> PostgreSQL driver', () => {
       },
     });
     await assert.rejects(async () => await driver2.query('SELECT x'));
-    const [ sql, err ] = result[0];
+    const [ err, sql ] = result[0];
     assert.equal(sql, 'SELECT x');
     assert.ok(err);
     assert.ok(/column "x" does not exist/.test(err.message));
