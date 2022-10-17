3b7d7c38dd1213fdb881fb7629d5ec627678d610
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2022-10-17 15:03:57 +0800

    docs: support switchable dark theme, at web app level

--- docs/assets/javascript/app.mjs
+++ docs/assets/javascript/app.mjs
@@ -6,6 +6,33 @@ function main() {
       if (trigger) trigger.classList.add('active');
     }
   });
+
+  const colorSchemes = ['os-default', 'light', 'dark'];
+
+  function switchColorScheme(value) {
+    if (colorSchemes.includes(value)) {
+      document.documentElement.classList.remove(...colorSchemes);
+      document.documentElement.classList.add(value);
+      try {
+        localStorage.setItem('color-scheme', value);
+      } catch (e) {}
+    }
+  }
+
+  let colorScheme = 'os-default';
+  try {
+    colorScheme = localStorage.getItem('color-scheme');
+  } catch (e) {}
+  switchColorScheme(colorScheme);
+
+  Array.from(document.querySelectorAll('#j-color-scheme'), function bind(el) {
+    el.addEventListener('change', function onChange(e) {
+      const value = e.target.value;
+      switchColorScheme(value);
+    });
+    const selected = el.querySelector(`option[value="${colorScheme}"]`);
+    if (selected) selected.selected = true;
+  });
 }
 
 main();
