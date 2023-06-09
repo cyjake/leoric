c410cfdc26fed0704c0daa222f6215797de2bd97
vagusX <vagusX@users.noreply.github.com>
2023-06-09 14:22:52 +0800

    fix: wrong export from sequelize (#391)

--- src/browser.ts
+++ src/browser.ts
@@ -3,7 +3,6 @@ const Spell = require('./spell');
 const Bone = require('./bone');
 const Collection = require('./collection');
 const { invokable: DataTypes, LENGTH_VARIANTS } = require('./data_types');
-const sequelize = require('./adapters/sequelize');
 const { heresql } = require('./utils/string');
 
 const Realm = require('./realm/base');
@@ -48,7 +47,7 @@ export { Collection };
 export { DataTypes };
 export { Logger };
 export { Spell };
-export { sequelize };
+export * from './adapters/sequelize';
 export { heresql };
 export * from './hint';
 export * from './decorators';
