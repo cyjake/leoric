ef47941c2606aab0789b9f90aaae0ef0332a9788
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2023-10-16 14:12:42 +0800

    docs: fix highlight of current aside menu item

--- docs/assets/javascript/app.mjs
+++ docs/assets/javascript/app.mjs
@@ -1,6 +1,6 @@
 function main() {
   Array.from(document.querySelectorAll('#header nav a'), function markActive(el) {
-    if (location.pathname.startsWith(el.getAttribute('href'))) {
+    if (location.pathname.replace(/\/$/, '') === el.getAttribute('href')) {
       el.classList.add('active');
       const trigger = el.closest('.dropdown-trigger');
       if (trigger) trigger.classList.add('active');
